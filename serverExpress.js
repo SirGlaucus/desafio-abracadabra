9deb66dd6fa2ad72e99d6d0179003dc2266b0515
SirGlaucus <glaucusjoseph@gmail.com>
2022-06-04 11:17:44 -0400

    Eliminando comillas, desafio terminado

--- serverExpress.js
+++ serverExpress.js
@@ -1,32 +1,32 @@
-const express = require("express")
+const express = require('express')
 
 const app = express()
 
 // Crear un servidor con Express en el puerto 3000
 app.listen(3000, () => {
-    console.log("El servidor está inicializado en el puerto 3000")
+    console.log('El servidor está inicializado en el puerto 3000')
 })
 
 //  Definir la carpeta “assets” como carpeta pública del servidor
-app.use(express.static("assets"))
+app.use(express.static('assets'))
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
 // Crear en el servidor un arreglo de nombres y devolverlo en formato JSON a través de la ruta /abracadabra/usuarios.
 
 const usuarios = {
-    usuarios: ["Juan", "Joselyn", "Astrid", "Maria", "Ignacia", "Javier", "Brain"]
+    usuarios: ['Juan', 'Joselyn', 'Astrid', 'Maria', 'Ignacia', 'Javier', 'Brain']
 }
-app.get("/abracadabra/usuarios", (req, res) => {
+app.get('/abracadabra/usuarios', (req, res) => {
     res.send(usuarios)
 })
 
 // Crear un middleware con la ruta /abracadabra/juego/:usuario para validar que el
 // usuario recibido como parámetro “usuario” existe en el arreglo de nombres creado en el servidor
 
-app.use("/abracadabra/juego/:usuario", (req, res, next) => {
+app.use('/abracadabra/juego/:usuario', (req, res, next) => {
 
     const nombre = req.params.usuario
 
@@ -41,14 +41,14 @@ app.use("/abracadabra/juego/:usuario", (req, res, next) => {
 })
 // En caso de ser exitoso, permitir el paso a la ruta GET correspondiente, de lo contrario devolver la imagen “who.jpeg”.
 
-app.get("/abracadabra/juego/:usuario", (req, res) => {
+app.get('/abracadabra/juego/:usuario', (req, res) => {
     const nombre = req.params.usuario
     res.send(nombre)
 })
 
 // Crear una ruta /abracadabra/conejo/:n que valide si el parámetro “n” coincide con el número generado de forma aleatoria.
 
-app.get("/abracadabra/conejo/:n", (req, res) => {
+app.get('/abracadabra/conejo/:n', (req, res) => {
     const numeroElegido = parseInt(req.params.n)
     const numeroAlAzar = Math.floor((Math.random() * 4) + 1)
     console.log(numeroAlAzar, numeroElegido)
@@ -60,7 +60,7 @@ app.get("/abracadabra/conejo/:n", (req, res) => {
 })
 
 // Crear una ruta genérica que devuelva un mensaje diciendo “Esta página no existe...” al consultar una ruta que no esté definida en el servidor.
-app.get("*", (req, res) => {
+app.get('*', (req, res) => {
     // Paso 2
-    res.send("<center><h1>Esta ruta no existe...</h1> </center>")
+    res.send('<center><h1>Esta ruta no existe...</h1> </center>')
 })
